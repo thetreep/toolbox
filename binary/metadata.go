6f57343a1229a2aa2092d9303316e49e377dba88
EarvinKayonga <demoneartin@gmail.com>
2020-09-09 18:07:58 +0200

    binary: add commit info to binary

---
+++ binary/metadata.go
@@ -0,0 +1,38 @@
+package binary
+
+import "time"
+
+// Metadata holds build metadatas.
+type Metadata struct {
+	Branch     string    `json:"branch"`
+	Compiler   string    `json:"compiler"`
+	CompiledAt string    `json:"compiledAt"`
+	Sha        string    `json:"sha"`
+	StartTime  time.Time `json:"started_at"`
+}
+
+// ToMap returns a map from given metadata.
+//nolint
+func (m Metadata) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"Branch":     m.Branch,
+		"Compiler":   m.Compiler,
+		"CompiledAt": m.CompiledAt,
+		"Sha":        m.Sha,
+		"StartAt":    m.StartTime,
+	}
+}
+
+var binaryMetadata = Metadata{
+	StartTime: time.Now(),
+} //
+
+// BinaryMetadata return metadata on the given binary.
+func BinaryMetadata() Metadata { // nolint
+	return binaryMetadata
+}
+
+// SetBinaryMetadata sets metadata.
+func SetBinaryMetadata(m Metadata) {
+	binaryMetadata = m
+}
