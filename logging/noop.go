dc28d0b0355c3a9076e83b08ee47990c1c8836bb
EarvinKayongaTheTreep <earvin.kayonga@thetreep.com>
2021-03-11 12:24:28 +0100

    remove ln logs

--- logging/noop.go
+++ logging/noop.go
@@ -2,28 +2,20 @@ package logging
 
 type Noop struct{}
 
-func NoopLogger() LibLogger                               { return &Noop{} }
-func (*Noop) Debugf(format string, args ...interface{})   {}
-func (*Noop) Infof(format string, args ...interface{})    {}
-func (*Noop) Printf(format string, args ...interface{})   {}
-func (*Noop) Warnf(format string, args ...interface{})    {}
-func (*Noop) Warningf(format string, args ...interface{}) {}
-func (*Noop) Errorf(format string, args ...interface{})   {}
-func (*Noop) Fatalf(format string, args ...interface{})   {}
-func (*Noop) Panicf(format string, args ...interface{})   {}
-func (*Noop) Debug(args ...interface{})                   {}
-func (*Noop) Info(args ...interface{})                    {}
-func (*Noop) Print(args ...interface{})                   {}
-func (*Noop) Warn(args ...interface{})                    {}
-func (*Noop) Warning(args ...interface{})                 {}
-func (*Noop) Error(args ...interface{})                   {}
-func (*Noop) Fatal(args ...interface{})                   {}
-func (*Noop) Panic(args ...interface{})                   {}
-func (*Noop) Debugln(args ...interface{})                 {}
-func (*Noop) Infoln(args ...interface{})                  {}
-func (*Noop) Println(args ...interface{})                 {}
-func (*Noop) Warnln(args ...interface{})                  {}
-func (*Noop) Warningln(args ...interface{})               {}
-func (*Noop) Errorln(args ...interface{})                 {}
-func (*Noop) Fatalln(args ...interface{})                 {}
-func (*Noop) Panicln(args ...interface{})                 {}
+func NoopLogger() LibLogger                             { return &Noop{} }
+func (*Noop) Debugf(format string, args ...interface{}) {}
+func (*Noop) Infof(format string, args ...interface{})  {}
+func (*Noop) Printf(format string, args ...interface{}) {}
+func (*Noop) Warnf(format string, args ...interface{})  {}
+func (*Noop) Errorf(format string, args ...interface{}) {}
+func (*Noop) Fatalf(format string, args ...interface{}) {}
+func (*Noop) Panicf(format string, args ...interface{}) {}
+func (*Noop) Debug(args ...interface{})                 {}
+func (*Noop) Info(args ...interface{})                  {}
+func (*Noop) Print(args ...interface{})                 {}
+func (*Noop) Warn(args ...interface{})                  {}
+func (*Noop) Warning(args ...interface{})               {}
+func (*Noop) Error(args ...interface{})                 {}
+func (*Noop) Fatal(args ...interface{})                 {}
+func (*Noop) Panic(args ...interface{})                 {}
+func (*Noop) Debugln(args ...interface{})               {}
