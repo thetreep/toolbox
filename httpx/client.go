fe75d05a8050227ac18c9bb98d1fbd6a6ad46b73
hectorj-thetreep <hector.jusforgues@thetreep.com>
2024-04-19 09:45:43 +0200

    feature(httpx): introduce NewClient func to create a non-global http.Client

--- httpx/client.go
+++ httpx/client.go
@@ -8,6 +8,7 @@ import (
 
 // DefaultClient is a default http.Client with
 // tracing enabled.
+// Deprecated: use NewClient instead
 // nolint
 var DefaultClient = &http.Client{
 	Timeout: time.Minute * 15,
@@ -17,4 +18,17 @@ var DefaultClient = &http.Client{
 		}).Dial,
 		TLSHandshakeTimeout: 15 * time.Second,
 	},
+}
+
+//nolint:exhaustruct // we don't need all fields
+func NewClient() *http.Client {
+	return &http.Client{
+		Timeout: time.Minute * 15,
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout: 20 * time.Second,
+			}).Dial,
+			TLSHandshakeTimeout: 15 * time.Second,
+		},
+	}
 }
