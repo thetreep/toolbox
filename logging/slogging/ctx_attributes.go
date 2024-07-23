3c50513e82319332ab629a153ef6530cc4b6afd3
hectorj-thetreep <hector.jusforgues@thetreep.com>
2024-07-23 18:08:18 +0200

    feature(slogging): introduce slogging package
    Helpers to
    
    - put the slogger in the ctx
    - put attributes in the context
    - put attributes in errors
    - set the log level of an error (mainly to log them as warnings)
    
    And global methods to log using all this ctx data from anywhere in your app.
    
    Bonus: a default noop logger

---
+++ logging/slogging/ctx_attributes.go
@@ -0,0 +1,35 @@
+package slogging
+
+import (
+	"context"
+	"log/slog"
+)
+
+func CtxWithLogAttributes(ctx context.Context, attrs ...slog.Attr) context.Context {
+	attributes, _ := ctx.Value(attributesCtxKey{}).(map[string]slog.Attr)
+	if attributes == nil {
+		attributes = make(map[string]slog.Attr)
+	}
+
+	for _, attr := range attrs {
+		attributes[attr.Key] = attr
+	}
+
+	return context.WithValue(ctx, attributesCtxKey{}, attributes)
+}
+
+type attributesCtxKey struct{}
+
+func getAttributesFromContext(ctx context.Context) []slog.Attr {
+	attributes, _ := ctx.Value(attributesCtxKey{}).(map[string]slog.Attr)
+	if attributes == nil {
+		return nil
+	}
+
+	attributesSlice := make([]slog.Attr, 0, len(attributes))
+	for _, attr := range attributes {
+		attributesSlice = append(attributesSlice, attr)
+	}
+
+	return attributesSlice
+}
