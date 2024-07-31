361bf94fcca0f8f3ebac3f0349784f3102516416
hectorj-thetreep <hector.jusforgues@thetreep.com>
2024-07-31 16:53:03 +0200

    feature(logger): add handler wrapper to fix log's "source" via the stored program counter

--- logger/log.go
+++ logger/log.go
@@ -9,6 +9,7 @@ import (
 )
 
 func Error(ctx context.Context, err error, attrs ...slog.Attr) {
+	ctx = storeProgramCounter(ctx, 1)
 	attrs = append(attrs, slog.Any("stacktrace", errtrace.FormatString(err)))
 	attrs = append(attrs, getAttributesFromErr(err)...)
 
@@ -16,22 +17,27 @@ func Error(ctx context.Context, err error, attrs ...slog.Attr) {
 }
 
 func ErrorAsWarning(ctx context.Context, err error, attrs ...slog.Attr) {
+	ctx = storeProgramCounter(ctx, 1)
 	Error(ctx, ErrWithLogLevel(err, slog.LevelWarn), attrs...)
 }
 
 func Info(ctx context.Context, msg string, attrs ...slog.Attr) {
+	ctx = storeProgramCounter(ctx, 1)
 	logAttrs(ctx, slog.LevelInfo, msg, attrs...)
 }
 
 func Debug(ctx context.Context, msg string, attrs ...slog.Attr) {
+	ctx = storeProgramCounter(ctx, 1)
 	logAttrs(ctx, slog.LevelDebug, msg, attrs...)
 }
 
 func Warn(ctx context.Context, msg string, attrs ...slog.Attr) {
+	ctx = storeProgramCounter(ctx, 1)
 	logAttrs(ctx, slog.LevelWarn, msg, attrs...)
 }
 
 func logAttrs(ctx context.Context, level slog.Level, msg string, attrs ...slog.Attr) {
+	ctx = storeProgramCounter(ctx, 1)
 	logger := GetLoggerFromContext(ctx)
 	attrs = append(attrs, getAttributesFromContext(ctx)...)
 
