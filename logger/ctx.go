3420ed21dde55e19ce34d10f1d1821eaa80cde8b
hectorj-thetreep <hector.jusforgues@thetreep.com>
2024-08-21 15:25:25 +0200

    feature(logger): add `CtxProxyHandler`

--- logger/ctx.go
+++ logger/ctx.go
@@ -3,6 +3,8 @@ package logger
 import (
 	"context"
 	"log/slog"
+
+	"braces.dev/errtrace"
 )
 
 func CtxWithLogger(ctx context.Context, logger *slog.Logger) context.Context {
@@ -19,3 +21,49 @@ func GetLoggerFromContext(ctx context.Context) *slog.Logger {
 }
 
 type loggerCtxKey struct{}
+
+// CtxProxyHandler is a slog.Handler which proxies all calls to
+// the handler stored in the context.
+//
+// It is an alternative to the global Error, Warn, Info and Debug functions
+type CtxProxyHandler struct {
+	withAttrs  []slog.Attr
+	withGroups []string
+}
+
+func (c CtxProxyHandler) getHandler(ctx context.Context) slog.Handler {
+	handler := GetLoggerFromContext(ctx).Handler().WithAttrs(c.withAttrs)
+	for _, group := range c.withGroups {
+		handler = handler.WithGroup(group)
+	}
+	return handler
+}
+
+func (c CtxProxyHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return c.getHandler(ctx).Enabled(ctx, level)
+}
+
+func (c CtxProxyHandler) Handle(ctx context.Context, record slog.Record) error {
+	ctx = storeProgramCounter(ctx, 1)
+	record.AddAttrs(getAttributesFromContext(ctx)...)
+	return errtrace.Wrap(c.getHandler(ctx).Handle(ctx, record))
+}
+
+func (c CtxProxyHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return CtxProxyHandler{
+		withAttrs:  append(c.withAttrs, attrs...),
+		withGroups: c.withGroups,
+	}
+}
+
+func (c CtxProxyHandler) WithGroup(name string) slog.Handler {
+	if name == "" {
+		return c
+	}
+	return CtxProxyHandler{
+		withAttrs:  c.withAttrs,
+		withGroups: append(c.withGroups, name),
+	}
+}
+
+var _ slog.Handler = (*CtxProxyHandler)(nil)
