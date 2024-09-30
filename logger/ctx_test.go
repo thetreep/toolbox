145be17a3c35b59d7f8793b508b413c9b2f4755e
Hector Jusforgues <134286056+hectorj-thetreep@users.noreply.github.com>
2024-09-30 15:39:54 +0200

    feature(logger): add `CtxProxyHandler` (#88)

--- logger/ctx_test.go
+++ logger/ctx_test.go
@@ -29,3 +29,25 @@ func TestCtxWithLogger(t *testing.T) {
 	ErrorAsWarning(ctx, errtrace.New("test"))
 	require.Len(t, handler.GetLogs(), 5)
 }
+
+func TestCtxProxyHandler(t *testing.T) {
+	handler := InMemoryHandler(slog.LevelDebug)
+	logger := slog.New(handler)
+	proxy := CtxProxyHandler{}
+	proxyLogger := slog.New(proxy)
+
+	proxyLogger.DebugContext(context.Background(), "test") // should be a no-op
+
+	ctx := context.WithValue(CtxWithLogger(context.Background(), logger), "toto", "tutu")
+
+	require.Empty(t, handler.GetLogs())
+	proxyLogger.DebugContext(ctx, "test")
+	require.Len(t, handler.GetLogs(), 1)
+	proxyLogger.InfoContext(ctx, "test")
+	require.Len(t, handler.GetLogs(), 2)
+	proxyLogger.WarnContext(ctx, "test")
+	require.Len(t, handler.GetLogs(), 3)
+	proxyLogger.ErrorContext(ctx, "test")
+	require.Len(t, handler.GetLogs(), 4)
+
+}
