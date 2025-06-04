9e116c127fb964e99c235b6d9ef9212bf6d992cc
fabianthetreep <117080986+fabianthetreep@users.noreply.github.com>
2025-06-04 15:50:02 +0200

    fix(automapper): do not prealocate value to get nil when giving nil as input

--- automapper/mapper.go
+++ automapper/mapper.go
@@ -7,9 +7,10 @@ func (a MapFunc[T, U]) Map(v T) U {
 }
 
 func (a MapFunc[T, U]) MapEach(v []T) []U {
-	values := make([]U, 0)
+	var values []U //nolint:prealloc // need to keep nil value
 	for _, value := range v {
 		values = append(values, a.Map(value))
 	}
+
 	return values
 }
