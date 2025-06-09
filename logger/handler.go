5a63ae3973638dea227baa4ed7e8ba35a8919e6b
fabianthetreep <117080986+fabianthetreep@users.noreply.github.com>
2025-06-09 10:46:50 +0200

    feat(logger): define a new slog.Handler that get log level from context (#109)
    * feat(logger): define a new slog.Handler that get log level from context
    
    * WIP: Hector fb
    
    * WIP: hector fb

---
+++ logger/handler.go
@@ -0,0 +1,25 @@
+package logger
+
+import (
+	"context"
+	"log/slog"
+)
+
+// ctxLogHandler implement slog.Handler,
+// it override Enabled method to get the log level from the context.
+type ctxLogHandler struct {
+	slog.Handler
+}
+
+func NewCtxLogHandler(wrappedHandler slog.Handler) slog.Handler {
+	return ctxLogHandler{Handler: wrappedHandler}
+}
+
+func (handler ctxLogHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	ctxLevel := LogLevelFromCtx(ctx)
+	if ctxLevel == nil {
+		return handler.Handler.Enabled(ctx, level)
+	}
+
+	return level >= *ctxLevel
+}
