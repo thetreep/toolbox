e7cb11ff9da4b2954441d6256b3274f756dd4ef9
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2026-01-13 09:04:16 +0100

    chore(config): onepassword

--- config/onepassword_secret.go
+++ config/onepassword_secret.go
@@ -8,30 +8,37 @@ import (
 )
 
 const (
-	OPSecretPrefix           = "op://"
+	OPSecretPrefix = "op://"
+	//nolint:gosec // not credentials
 	OPServiceAccountTokenKey = "OP_SERVICE_ACCOUNT_TOKEN"
 )
 
 // ResolveSecretFromOP takes a configuration value and, if it starts with OPSecretPrefix, replaces it with the secret value.
-func ResolveSecretFromOP(ctx context.Context, value string, options ...resolveSecretFromOPOption) (string, error) {
+func ResolveSecretFromOP(ctx context.Context, value string, options ...OPOption) (string, error) {
 	// just return the value if it doesn't start with the prefix
 	if !strings.HasPrefix(value, OPSecretPrefix) {
 		return value, nil
 	}
 
+	opts := &opOptions{}
+	for _, option := range options {
+		option(opts)
+	}
+
 	// token to use for authentication
-	token, err := getToken(options...)
+	token, err := getToken(opts)
+	if err != nil {
+		return "", err
+	}
 
 	// parse options
 	integrationName := "unkown"
+	if opts.integrationName != "" {
+		integrationName = opts.integrationName
+	}
 	integrationVersion := "unkown"
-	for _, option := range options {
-		switch {
-		case option.integrationName != "":
-			integrationName = option.integrationName
-		case option.integrationVersion != "":
-			integrationVersion = option.integrationVersion
-		}
+	if opts.integrationVersion != "" {
+		integrationVersion = opts.integrationVersion
 	}
 
 	// fetch the secret
@@ -46,45 +53,42 @@ func ResolveSecretFromOP(ctx context.Context, value string, options ...resolveSe
 	return client.Secrets().Resolve(ctx, value)
 }
 
-type resolveSecretFromOPOption struct {
+type opOptions struct {
 	token              string
 	integrationName    string
 	integrationVersion string
 }
 
+type OPOption func(o *opOptions)
+
 // WithOPToken sets the token to use for authentication.
-func WithOPToken(token string) resolveSecretFromOPOption {
-	return resolveSecretFromOPOption{token: token}
+func WithOPToken(token string) OPOption {
+	return func(o *opOptions) {
+		o.token = token
+	}
 }
 
 // WithIntegrationName sets the name of the integration e.g. consumer app name.
-func WithIntegrationName(name string) resolveSecretFromOPOption {
-	return resolveSecretFromOPOption{integrationName: name}
+func WithIntegrationName(name string) OPOption {
+	return func(o *opOptions) {
+		o.integrationName = name
+	}
 }
 
 // WithIntegrationVersion sets the version of the integration e.g. consumer app version.
-func WithIntegrationVersion(version string) resolveSecretFromOPOption {
-	return resolveSecretFromOPOption{integrationVersion: version}
+func WithIntegrationVersion(version string) OPOption {
+	return func(o *opOptions) {
+		o.integrationVersion = version
+	}
 }
 
 // getToken to use for authentication.
 // Defaults to OP_SERVICE_ACCOUNT_TOKEN env var.
 // Otherwise, needs to be set with WithOPToken.
-func getToken(options ...resolveSecretFromOPOption) (string, error) {
-	var token string
-	for _, option := range options {
-		if option.token != "" {
-			token = option.token
-		}
+func getToken(options *opOptions) (string, error) {
+	if options.token != "" {
+		return options.token, nil
 	}
 
-	if token == "" {
-		var err error
-		token, err = GetEnvOrError(OPServiceAccountTokenKey)
-		if err != nil {
-			return "", err
-		}
-	}
-
-	return token, nil
+	return GetEnvOrError(OPServiceAccountTokenKey)
 }
